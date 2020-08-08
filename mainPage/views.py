8e2ba0f64df2f74f4a475a8c7d190d9260fb7ceb
oereo <dlstpgns0406@gmail.com>
2020-08-09 06:28:21 +0900

    [ADD]

--- mainPage/views.py
+++ mainPage/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render, HttpResponse
-import random, json
+import random
+import json
 
 import requests
 from bs4 import BeautifulSoup as BS
 
-url = 'http://www.weather.go.kr/weather/observation/currentweather.jsp'
-
-response = requests.get(url)
-
-if(response.status_code != 200):
-    print("%d 에러가 발생하였습니다" % response.status_code)
-    quit()
-
-soup = BS(response.content, 'html.parser')
-table = soup.find('table', {'class':'table_develop3'})
-
-data = []
-for tr in table.find_all('tr'):
-    tds = list(tr.find_all('td'))
-    for td in tds:
-        if td.find('a'):
-            point = td.find('a').text
-            temperature = tds[5].text
-            humidity = tds[9].text
-            data.append([point, temperature, humidity])
 
 # with open('weather.csv') as file:
 #     file.write('지역, 기온, 습도 \n')
@@ -31,7 +12,7 @@ for tr in table.find_all('tr'):
 #         file.write('{0}, {1}, {2} \n'.format(i[0], i[1], [2]))
 # Create your views here.
 def data_json(request):
-    
+
     Fluctuation_ratio = 50  # 등락비율(%)
 
     ratio = Fluctuation_ratio / float(100)
@@ -40,9 +21,9 @@ def data_json(request):
 
     Counts = [None, ]
 
-    Costs = ['온도',]
+    Costs = ['온도', ]
 
-    for i in range(1,31):
+    for i in range(1, 31):
 
         Counts.append(str(i))
 
@@ -70,34 +51,28 @@ def data_json(request):
 
     }
 
-    return HttpResponse(json.dumps(data),content_type='text/json')
+    return HttpResponse(json.dumps(data), content_type='text/json')
 
- 
 
 def home(request):
-    return render(request,'home.html')
+    url = 'http://www.weather.go.kr/weather/observation/currentweather.jsp'
+    response = requests.get(url)
+    if(response.status_code != 200):
+        print("%d 에러가 발생하였습니다" % response.status_code)
+        quit()
+
+    soup = BS(response.content, 'html.parser')
+    table = soup.find('table', {'class': 'table_develop3'})
+
+    data_temp = []
+    for tr in table.find_all('tr'):
+        tds = list(tr.find_all('td'))
+        for td in tds:
+            if td.find('a'):
+                point = td.find('a').text
+                temperature = tds[5].text
+                humidity = tds[9].text
+                data_temp.append([point, temperature, humidity])
+    return render(request, "home.html", {'data_temp':data_temp})
 
 
-    
-
-# def home(request):
-#     template_data = {}
-#     time_unit = [i for i in range(1,31)]
-#     data1 = np.random.randint(0, high=40, size=30).tolist
-
-#     template_data = {
-#         'day': [1,2,3],
-#         'temperature': [29,27,33],
-#         'time_unit': time_unit,
-#         'data1': data1,
-        
-#     }
-
-#     if request.method == 'GET':
-#         print("get...")
-#     return render(request, 'home.html', template_data)
-
-
-
-#def home(request):
-#    return render(request, 'home.html')
