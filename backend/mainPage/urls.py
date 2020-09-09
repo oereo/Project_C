c04033cca7c2b7f5aecf18944b8997bbb9d83fcf
oereo <dlstpgns0406@gmail.com>
2020-09-10 05:18:14 +0900

    [ADD] Template

--- backend/mainPage/urls.py
+++ backend/mainPage/urls.py
@@ -3,5 +3,15 @@ from django.urls import path, include
 from mainPage import views
 
 urlpatterns = [
-    path('', views.home, name ="home"),
+    path('', views.index, name ="index"),
+    path('dashboard/', views.dashboard, name ="dashboard"),
+    path('icons/', views.icons, name ="icons"),
+    path('login/', views.login, name ="login"),
+    path('map/', views.map, name ="map"),
+    path('maps/', views.maps, name ="maps"),
+    path('profile/', views.profile, name ="profile"),
+    path('register/', views.register, name ="register"),
+    path('tables/', views.tables, name ="tables"),
+    path('upgrade/', views.upgrade, name ="upgrade"),
+
 ]
--- backend/mainPage/views.py
+++ backend/mainPage/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
+def icons(request):
+    return render(request, 'icons.html')
+def login(request):
+    return render(request, 'login.html')
+def map(request):
+    return render(request, 'map.html')
+def maps(request):
+    return render(request, 'maps.html')
+def profile(request):
+    return render(request, 'profile.html')
+def register(request):
+    return render(request, 'register.html')
+def tables(request):
+    return render(request, 'tables.html')
+def upgrade(request):
+    return render(request, 'upgrade.html')
