1ac5e15a6a42bc1f66f2ac122319486351617b57
oereo <dlstpgns0406@gmail.com>
2020-09-11 22:57:14 +0900

    [ADD] Admin.py

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -3,5 +3,9 @@ from .models import User
 from django.contrib.auth.models import Group
 
 # Register your models here.
-admin.site.register(User)
+class userAdmin(admin.ModelAdmin):
+    list_display = ['email', 'nickname', 'business_number']
+
+admin.site.register(User, userAdmin)
+
 admin.site.unregister(Group)
